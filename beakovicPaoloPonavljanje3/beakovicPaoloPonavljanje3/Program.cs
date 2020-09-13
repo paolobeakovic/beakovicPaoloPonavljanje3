182241bd5704faea3d4823da2cd1e3faf75911e2
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-09-13 23:12:52 +0200

    Add files via upload

---
+++ beakovicPaoloPonavljanje3/beakovicPaoloPonavljanje3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaoloPonavljanje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = Convert.ToInt32(Console.ReadLine());
+            if (broj % 4 == 0 && broj % 6 == 0) Console.WriteLine("Broj je dijeljiv sa 4 i sa 6");
+            else if (broj % 4 == 0) Console.WriteLine("Broj je dijeljiv sa 4 ");
+            else if (broj % 6 == 0) Console.WriteLine("Broj je dijeljiv sa 6 ");
+            else if (broj % 4 != 0 && broj % 6 != 0) Console.WriteLine("Broj nije dijeljiv sa 4 ni sa 6 ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
